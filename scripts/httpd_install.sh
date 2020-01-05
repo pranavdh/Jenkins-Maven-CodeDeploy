bb40a4efcb6694f155c6d20f4e8f8a6c242a1da0
pd <pranavunixjayant@gmail.com>
2020-01-05 10:04:56 +0530

    changes made to httpd_install.sh

--- scripts/httpd_install.sh
+++ scripts/httpd_install.sh
@@ -2,4 +2,8 @@
 yum install httpd -y
 cd /var/www/html/
 rm -rf /var/www/html/vhosts/cloudeureka.online/index.php
-cp -prf ./index.html /var/www/html/vhosts/cloudeureka.online/index.html
+cd /var/www/html/vhosts/cloudeureka.online/
+
+cat <<EOF > index.html
+This is CloudEureka.Online:V1
+EOF
