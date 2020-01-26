4d65de04062e62fb37b94efaf1b98822c583d63c
root <root@ip-10-0-12-31.ap-southeast-2.compute.internal>
2020-01-26 16:29:10 +0000

    changed scripts httpd_install.sh

--- scripts/httpd_install.sh
+++ scripts/httpd_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 yum install httpd -y
 cd /var/www/html/
-rm -rf /var/www/html/vhosts/cloudeureka.online/index.php
-cat <<EOF > /var/www/html/vhosts/cloudeureka.online/index.html
-This is CloudEureka.Online:V2
+cat <<EOF > /var/www/html/index.html
+Hello World:V1
 EOF
